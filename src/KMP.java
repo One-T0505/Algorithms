ee8478ca141fda749a6201362cb588d628d8b35f
oneT <15549316580@163.com>
2022-10-14 13:17:18 +0800

    KMP算法

--- src/KMP.java
+++ src/KMP.java
@@ -47,4 +47,28 @@ public class KMP {
         }
         return next;
     }
+
+
+    // KMP应用：假如给定字符串s1，s2，判断s2是不是s1的旋转子串。
+    // 旋转子串的定义：假如s1=abcdef  那么s1的旋转子串有：bcdefa  cdefab  defabc  efabcd  fabcde  abcdef
+
+    // 思路：可以将原始串复制一份拼成两倍长度：s1=abcdefabcdef  然后让s2用KMP算法去匹配s1。当长度变为两倍时，所有旋转子串
+    //      都是其子串。
+
+    public static boolean isRotatedSubString(String s1, String s2){
+        if (s1 == null || s2 == null || s1.length() != s2.length())
+            return false;
+        // 扩充原始串
+        int N = s1.length();
+        String processed = s1.repeat(2);
+        return kmp(processed, s2) != -1;
+    }
+
+
+    public static void main(String[] args) {
+        String s1 = "abcdef";
+        String s2 = "abcdef";
+        System.out.println(isRotatedSubString(s1, s2));
+    }
+
 }
