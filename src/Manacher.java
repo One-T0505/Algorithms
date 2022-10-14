51996b72854445505ce64aff2b8528ce85b44855
oneT <15549316580@163.com>
2022-10-14 13:18:27 +0800

    Manacher算法

--- src/Manacher.java
+++ src/Manacher.java
@@ -1,15 +1,4 @@
 public class Manacher {
-    // 将字符串扩充为辨准处理串：1221 --> #1#2#2#1#
-    public static char[] process(String s){
-        char[] res = new char[2 * s.length() + 1];
-        char[] ori = s.toCharArray();
-        int index = 0;
-        for (int i = 0; i < res.length; i++) {
-            res[i] = (i & 1) == 1 ? ori[index++] : '#';
-        }
-        return res;
-    }
-
     public static int manacher(String s){
        if (s == null || s.length() == 0)
            return 0;
@@ -21,8 +10,8 @@ public class Manacher {
             // 先确定当前元素至少的回文区域。如果R > i，不管落入4种情况的哪一种，都是 Math.min(res[2 * C - i], R - i)
             res[i] = R > i ? Math.min(res[2 * C - i], R - i) : 1;
 
-            // 4种情况中，只有两种需要左右扩充，但是如果将扩充算法写到对应的情况下会代码冗余，所以写成如下这样，4中情况都会进入该
-            // 扩充算法，但是原有的不需要扩充的两种情况进入该循环后会直接失败，所以效果等价。
+            // 4种情况中，只有两种需要左右扩充，但是如果将扩充算法写到对应的情况下会代码冗余，所以写成如下这样，4种情况
+            // 都会进入该扩充算法，但是原有的不需要扩充的两种情况进入该循环后会直接失败，所以效果等价。
             while (i + res[i] < ori.length && i - res[i] > -1){
                 if (ori[i + res[i]] == ori[i - res[i]])
                     res[i]++;
@@ -39,6 +28,17 @@ public class Manacher {
         }
         // 经发现：标准流字符串中的回文半径-1，就可以得到原始字符串中的回文直径
         return max - 1;
+    }
+
+    // 将字符串扩充为辨准处理串：1221 --> #1#2#2#1#
+    private static char[] process(String s){
+        char[] res = new char[2 * s.length() + 1];
+        char[] ori = s.toCharArray();
+        int index = 0;
+        for (int i = 0; i < res.length; i++) {
+            res[i] = (i & 1) == 1 ? ori[index++] : '#';
+        }
+        return res;
     }
 
     public static void main(String[] args) {
